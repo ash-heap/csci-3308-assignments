418e6a4bf75921ea9f0a41aae2057bf22abbb881
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2013-09-12 07:35:12 -0600

    Add framework for the Dessert class.

---
+++ 02-programming_in_ruby/assignment2.rb
@@ -0,0 +1,24 @@
+# File: assignemnt2.rb
+# Written: 09/12/2013.255 - Michael R. Shannon
+#
+
+
+
+
+
+
+### Part 1 - Classes
+
+# a) Dessert Class
+class Dessert
+  def initialize(name, calories)
+  end
+  def healthy?
+  end
+  def delicious?
+  end
+end
+
+
+
+
