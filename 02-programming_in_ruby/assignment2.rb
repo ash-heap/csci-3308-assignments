66c7cc6aa40b8ee9303cdd57fbbb8007b4729b6e
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2013-09-12 08:46:27 -0600

    Complete the Dessert class.

--- 02-programming_in_ruby/assignment2.rb
+++ 02-programming_in_ruby/assignment2.rb
@@ -1,4 +1,4 @@
-# File: assignemnt2.rb
+# File: assignment.rb
 # Written: 09/12/2013.255 - Michael R. Shannon
 #
 
@@ -7,18 +7,34 @@
 
 
 
-### Part 1 - Classes
+# Part 1 - Classes
 
-# a) Dessert Class
+## a) Dessert Class
+
+### code
 class Dessert
   def initialize(name, calories)
+    @name = name
+    @calories = calories
   end
+  attr_accessor :name, :calories
   def healthy?
+    @calories < 200
   end
   def delicious?
+    true
   end
 end
 
+### tests
+cake      = Dessert.new('Cake', 400)
+apple     = Dessert.new('Apple', 100)
+ice_cream = Dessert.new('Ice Cream', 300)
+yogurt    = Dessert.new('Yogurt', 150)
+raise unless cake.delicious?      && !cake.healthy?
+raise unless apple.delicious?     && apple.healthy?
+raise unless ice_cream.delicious? && !ice_cream.healthy?
+raise unless yogurt.delicious?    && yogurt.healthy?
 
 
 
